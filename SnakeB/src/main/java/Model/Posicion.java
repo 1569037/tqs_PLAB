342e9d7d2baacf82ee4e729d1464254783ae0c86
1569037 <evelynramvar26@gmail.com>
2021-11-13 18:49:06 +0100

    arreglos

--- SnakeB/src/main/java/Model/Posicion.java
+++ SnakeB/src/main/java/Model/Posicion.java
@@ -8,7 +8,18 @@ public class Posicion {
 
     public Posicion(int x, int y, int max)
     {
-        //comprobar que la posicion no esta out of range
+        if(max!=20 && max!=15 && max!=10)
+            this.max = 20;
+        else
+            this.max = max;
+        if(x<this.max && x>= 0)
+            this.x=x;
+        else
+            this.x=0;
+        if(y<=this.max && y>=0)
+            this.y=y;
+        else
+            this.y=0;
     }
 
     public int getX() {
--- SnakeB/src/test/java/AppTest.java
+++ SnakeB/src/test/java/AppTest.java
@@ -1,44 +1,35 @@
-import Model.*;
-import junit.framework.TestCase;
+import Model.Posicion;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import static junit.framework.Assert.*;
+
 
 public class AppTest{
 
     @Test
     public void TestCrearPosicion() //pairwise
     {
-
         //proba caixa negra pairwise
-        int x = 11;
-        int y = 19;
-        int max = 15;
         Posicion pos1 = new Posicion(11,19, 15); //maxInterior - xInterior - yFront
         Posicion pos2 = new Posicion(19, 13, 20); //maxFront - xFront - yInterior
         Posicion pos3 = new Posicion(-1, 23, 19); //maxLim - xInvalid - yFora
         Posicion pos4 = new Posicion(18, -1, 31); //maxFora- xLim - yInvalid
         Posicion pos5 = new Posicion(31, 18, -1); //maxInvalid - xFora - yLim
 
-        assertNull(pos1);
-        assertEquals(pos1.getX(),x);
-        assertEquals(pos1.getY(),y);
-        assertEquals(pos1.getMax(), max);
+        Assertions.assertNotNull(pos1);
 
-        TestCrearPos(pos1.getX(),pos1.getY(), pos1.getMax());
-        TestCrearPos(pos2.getX(),pos2.getY(), pos2.getMax());
-        TestCrearPos(pos3.getX(),pos3.getY(), pos3.getMax());
-        TestCrearPos(pos4.getX(),pos4.getY(), pos4.getMax());
-        TestCrearPos(pos5.getX(),pos5.getY(), pos5.getMax());
 
+        Assertions.assertTrue(TestCrearPos(pos1));
+        Assertions.assertTrue(TestCrearPos(pos2));
+        Assertions.assertFalse(TestCrearPos(pos3));
+        Assertions.assertFalse(TestCrearPos(pos4));
+        Assertions.assertFalse(TestCrearPos(pos5));
     }
-    @Test
-    public void TestCrearPos(int x, int y, int max)
+
+
+    public boolean TestCrearPos(Posicion p)
     {
-        assertFalse("El valor x no es correcto", x< 0 || x>= max);
-        assertFalse("El valor y no es correcto", y < 0 || y >= max);
-        assertFalse("El valor de max no es correcto", max>20 || max<10);
+        return p.getX() < 0 || p.getX() >= p.getMax() || p.getY() < 0 || p.getY() >= p.getMax() || p.getMax() > 20 || p.getMax() < 10;
     }
-
 
     @Test
     public void TestCreacionComida()
